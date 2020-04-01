b235bcd3684094f8ad8466ff30988f3ec684ad03
orbea <orbea@riseup.net>
2020-04-01 14:57:36 -0700

    Add sprint_stamina_heal.
    This sets the amount to heal when not sprinting.

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ local dir           = minetest.is_yes(setting_get("sprint_forward_only", "false"
 local particles     = tonumber(setting_get("sprint_particles", "2"))
 local stamina       = minetest.is_yes(setting_get("sprint_stamina", "true"))
 local stamina_drain = tonumber(setting_get("sprint_stamina_drain", "2"))
+local stamina_heal  = tonumber(setting_get("sprint_stamina_heal", "2"))
 local replenish     = tonumber(setting_get("sprint_stamina_replenish", "2"))
 local starve        = minetest.is_yes(setting_get("sprint_starve", "true"))
 local starve_drain  = tonumber(setting_get("sprint_starve_drain", "0.5"))
@@ -90,7 +91,7 @@ end
 local function replenish_stamina(player)
   local player_stamina = player:get_meta():get_float("hbsprint:stamina")
   if player_stamina < 20 then
-    player_stamina = math.min(20, player_stamina + stamina_drain)
+    player_stamina = math.min(20, player_stamina + stamina_heal)
     player:get_meta():set_float("hbsprint:stamina", player_stamina)
   end
   if mod_hudbars then
